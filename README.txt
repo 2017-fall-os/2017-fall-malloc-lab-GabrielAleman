f91e1a6d87a1500b7a367e73c9937a26a2728fc3
Gabriel Aleman <galeman2@hotmail.com>
2017-12-05 10:18:30 -0700

    modifed testcases

--- README.txt
+++ README.txt
@@ -3,7 +3,17 @@ This directory contains:
 myAllocator.c: a first-fit allocator
 myAllocator.h: its header file
 
-myAllocatorTest1.c: a test program for my allocator 
+myA.c: is a copy of myAllocator.c with some modification of the lab. Things modified are or added
+       resizeRegion();
+          -added so that if next region is free and it has enough memory it resizes the region
+	  instead of making a new block at the end
+       bestFit();
+          -finds the best region if any to insert the block and returns the block address
+       bestFitAllocRegion()
+          -inserts block while calling bestFit();
+
+myAllocatorTest1.c: a test program that uses myA.c and shows a an example of best fit and resize
+resize region
 
 malloc.c: a replacement for malloc that uses my allocator
 test1.c: a test program that uses this replacement malloc
--- myAllocatorTest1.c
+++ myAllocatorTest1.c
@@ -19,20 +19,49 @@ void getutime(struct timeval *t)
 
 int main() 
 {
-  void *p1, *p2, *p3;
+  void *p1, *p2, *p3, *p4, *p5,*p6,*p7;
   arenaCheck();
   p1 = firstFitAllocRegion(254);
   arenaCheck();
-  p2 = firstFitAllocRegion(25400);
+  p2 = firstFitAllocRegion(180);
   arenaCheck();
   p3 = firstFitAllocRegion(254);
-  printf("%8zx %8zx %8zx\n", p1, p2, p3);
+  //printf("%8zx %8zx %8zx\n", p1, p2, p3);
+  arenaCheck();
+  //freeRegion(p2);
+  //arenaCheck();
+  //p1 = resizeRegion(p1,1000);
+  //arenaCheck();
+  p4 = firstFitAllocRegion(100);
+  arenaCheck();
+  p5 = firstFitAllocRegion(254);
   arenaCheck();
   freeRegion(p2);
   arenaCheck();
-  freeRegion(p3);
+  freeRegion(p4);
+  arenaCheck();
+  printf("\n This is bestFit of 100 gets placed in 4 slot\n\n");
+  p6 = bestFitAllocRegion(100); //it gets added to best fit which is where p4 use to be
+  arenaCheck();
+  freeRegion(p6);
+  arenaCheck();
+  printf("\n This is firstFit of 100 gets placed in 2 slot\n\n");
+  p6 = bestFitAllocRegion(100);
+  arenaCheck();
+  printf("\n This is resize of 100 to 168 gets placed in 2 slot insted of bottom\n\n");
+  p6 = resizeRegion(p6,168);//if you put any higher than 168 it will allocate in new memory slot becuase currently it has 104 and since next block only has 64 usable mem while the rest is use for prefix and sufix a total of 184. so when allocated 168 it will dispaly 184 since it getting rid of one prefix and sufix for extra 16 mem
+  arenaCheck();
+  p7 = bestFitAllocRegion(2000);
+  arenaCheck();
+  freeRegion(p6);
   arenaCheck();
   freeRegion(p1);
+  arenaCheck();
+  freeRegion(p3);
+  arenaCheck();
+  freeRegion(p5);
+  arenaCheck();
+  freeRegion(p7);
   arenaCheck();
   {				/* measure time for 10000 mallocs */
     struct timeval t1, t2;
